89650f83b859d72f75700a3e96d2570e7200dda1
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-28 23:00:07 +0300

    finished project

---
+++ src/features/budgets/budgetsSlice.jsx
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CATEGORIES = [
+  "housing",
+  "food",
+  "transportation",
+  "utilities",
+  "clothing",
+  "healthcare",
+  "personal",
+  "education",
+  "entertainment",
+];
+const initialState = CATEGORIES.map((category) => ({
+  category: category,
+  amount: 0,
+}));
+
+export const budgetsSlice = createSlice({
+  name: 'budgets',
+  initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      const newBudgets = state.map((budget) => {
+        if (budget.category === action.payload.category) {
+          return action.payload;
+        }
+        return budget;
+      });
+      return newBudgets;
+    },
+  },
+})
+
+export const selectBudgets = (state) => state.budgets;
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './app/App.jsx'
+import { Provider } from 'react-redux'
+import store from './app/store.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
